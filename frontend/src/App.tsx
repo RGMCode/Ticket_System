999a980f2e890250ba9dcba7ac8a203f0242f20b
RGMCode <rgmengedoth@gmail.com>
2023-08-29 14:45:17 +0200

    add react-router, favicon and SetUp.md

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
-
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Homepage from "./Pages/Homepage/Homepage.tsx";
+import LoginPage from "./Pages/LoginPage/LoginPage.tsx";
+import {useState} from "react";
+import RegisterPage from "./Pages/RegisterPage/RegisterPage.tsx";
 
 function App() {
 
-  return (
-    <>
-      <div>Hello Ticket System</div>
-    </>
-  )
+    const [user, setUser] = useState("")
+
+    return (
+        <>
+            <Routes>
+                <Route path={"/"} element={<Homepage/>}/>
+                <Route path={"/login"} element={<LoginPage setUser={setUser}/>}/>
+                <Route path={'/register'} element={<RegisterPage/>}/>
+            </Routes>
+        </>
+    );
 }
 
 export default App

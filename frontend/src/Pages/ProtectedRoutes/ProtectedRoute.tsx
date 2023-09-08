ab39518a2d4c586140f4fa90a7d49475513530cd
RGMCode <rgmengedoth@gmail.com>
2023-09-08 15:11:14 +0200

    MVP-clean up version

--- frontend/src/Pages/ProtectedRoutes/ProtectedRoute.tsx
+++ frontend/src/Pages/ProtectedRoutes/ProtectedRoute.tsx
@@ -1,13 +1,25 @@
-
-import { useUser } from '../../UserContext';
+import './ProtectedRoute.css';
+import {useUser} from '../../UserContext';
 import {Outlet} from "react-router-dom";
+import {Alert} from "react-bootstrap";
+
 
 export default function ProtectedRoute() {
-    const { user } = useUser();
+    const {user} = useUser();
 
     if (!user) {
-        return 'Sie sind nicht angemeldet.';
+        return <div className={"alert"}>
+            <div>
+                <Alert variant={"danger"}>
+                    <h1>Sie sind nicht mehr angemeldet!</h1>
+                    <br/>
+                    bitte klicken sie
+                    <Alert.Link href="/"> hier </Alert.Link>
+                    um auf die Hauptseite zu gelangen
+                </Alert>
+            </div>
+        </div>
     }
 
-    return <Outlet />;
+    return <Outlet/>;
 }

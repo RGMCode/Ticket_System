24a805ad29e0d4d5031b3093bd87e4162b47f039
RGMCode <rgmengedoth@gmail.com>
2023-09-20 15:37:01 +0200

    some css and change pictures

--- frontend/src/components/Carousels/Carousels.tsx
+++ frontend/src/components/Carousels/Carousels.tsx
@@ -1,9 +1,9 @@
 import './Carousels.css'
 import Carousel from 'react-bootstrap/Carousel';
 
-import HQ from '../../pictures/HQ.jpg'
-import HQ_Future from '../../pictures/HQ_Future.jpg'
-import DoWhatIsGreat from '../../pictures/DoWhatIsGreat.jpg'
+import SCC from '../../pictures/saas-concept-collage.jpg'
+import GT from '../../pictures/GreenTech.jpg'
+import hwg from '../../pictures/house-with-green-led-strip-that-says-smart-house.jpg'
 
 export default function Carousels() {
 
@@ -13,35 +13,36 @@ export default function Carousels() {
                 <Carousel.Item interval={4000} >
                     <img
                         className="d-block w-100 carousel-image"
-                        src={HQ}
+                        src={SCC}
                         alt="HQ"
                     />
                     <Carousel.Caption>
-                        <h5>Headquarter</h5>
+                        <h5>Future CloudGreen</h5>
+                        <p>Image by <a href="https://www.freepik.com/free-photo/saas-concept-collage_26301279.htm#query=cloud%20tech&position=2&from_view=keyword&track=ais">Freepik</a></p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item interval={4000} >
                     <img
                         className="d-block w-100 carousel-image"
-                        src={HQ_Future}
+                        src={GT}
                         alt="HQ_Future"
                     />
                     <Carousel.Caption>
-                        <h5>Headquarters of the future</h5>
-                        <p>This is what our sustainable future together looks like.</p>
+                        <h5>Green Tech</h5>
+                        <p>Image by <a href="https://www.freepik.com/free-vector/technological-ecology-concept_6849678.htm#query=green%20technology&position=2&from_view=search&track=ais">Freepik</a></p>
                     </Carousel.Caption>
                 </Carousel.Item>
 
                 <Carousel.Item interval={4000}>
                     <img
                         className="d-block w-100 carousel-image"
-                        src={DoWhatIsGreat}
+                        src={hwg}
                         alt="DoWhatIsGreat"
                     />
                     <Carousel.Caption>
-                        <h5>DWIG</h5>
-                        <p>Do what is great!</p>
+                        <h5>Our Future</h5>
+                        <a href="https://www.freepik.com/free-photo/house-with-green-led-strip-that-says-smart-house_41368739.htm#query=future%20city&position=49&from_view=search&track=ais">Image by chandlervid85</a> on Freepik
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
